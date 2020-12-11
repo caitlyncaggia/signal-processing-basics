e3600b8f0e6518aa8534eeb2cd631127c3aa1878
Caitlyn Caggia <caggiac@gmail.com>
2020-12-10 23:59:33 -0500

    Removing old files

--- Lab 5 - Chirp Aliasing/Lab5HW.m
+++
@@ -1,4 +0,0 @@
-%Lab 5 Homework
-[xx, fs] = audioread('CaityAHH.wav');
-Lsect = fs * 0.25;
-dd = plotspecDB(xx(:,1), fs, Lsect, 80);
--- Lab 8 - Spectrograms/hw8.m
+++
@@ -1,6 +0,0 @@
-
-ww = -pi:(pi/100):pi;
-bb = [0,.25,-1,1.5,-1,.25];
-HH = freqz(bb,1,ww);
-
-plot(ww,abs(HH))
